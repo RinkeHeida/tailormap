fab4867e66efddcf08045cc1b685c15a265ae7e3
mvdstruijk <m_struijk1990@hotmail.nl>
2020-03-05 14:46:21 +0100

    contentbottom is not used

--- viewer/src/main/webapp/viewer-html/common/viewercontroller/ol/OlComponent.js
+++ viewer/src/main/webapp/viewer-html/common/viewercontroller/ol/OlComponent.js
@@ -25,20 +25,8 @@ Ext.define("viewer.viewercontroller.ol.OlComponent", {
      * @param frameworkObject the implementing openlayers object
      */
     constructor: function (conf, frameworkObject) {
-
         viewer.viewercontroller.ol.OlComponent.superclass.constructor.call(this, conf);
         this.frameworkObject = frameworkObject;
-        if (conf.regionName === "content_bottom" && this.config.viewerController.mapComponent.contentBottom) {
-            // Make a new div and append it to the contentBottom div. So controls can't override other controls (for example the mouseposition renders itself to the content of this.div
-            var newDiv = document.createElement('div');
-            newDiv.id = conf.id + "content_bottom";
-
-            //frameworkObject.target.appendChild(newDiv);
-            frameworkObject.setTarget(newDiv);
-            if (conf.cssClass) {
-                newDiv.setAttribute("class", conf.cssClass);
-            }
-        }
     },
     /**
      * Can be overwritten to do something after the component is added.
